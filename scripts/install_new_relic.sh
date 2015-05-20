e681d1a0f8dd1069d165ad86f375c585b8790ca2
Bermudez, Jaime <jaime_bermudez@harvard.edu>
2015-05-20 13:33:22 -0400

    Added new relic keys.

--- scripts/install_new_relic.sh
+++ scripts/install_new_relic.sh
@@ -4,5 +4,5 @@ rpm -Uvh http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarc
 yum install -y newrelic-php5 || true
 
 export NR_INSTALL_SILENT=true
-export NR_INSTALL_KEY=your_new_relic_key_here
+export NR_INSTALL_KEY=35b61e138f7b613c6f0918f591b22ddef6857cf0
 newrelic-install install
